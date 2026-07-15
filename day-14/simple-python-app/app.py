6541c02b69ce78dc23b93501354ea670aee0aa17
Parvatha reddy <56736219+raghava18@users.noreply.github.com>
2026-07-15 18:45:54 +0530

    Change greeting message in Flask app

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Ome Shree Poojaya Raghavendra Satya Dharma Ratayacha bhajatam kalparukshaya Namatama Kamdhenuve'
 
 if __name__ == '__main__':
     app.run()
