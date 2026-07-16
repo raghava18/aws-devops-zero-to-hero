0626930873f6300978dfb3565d54e4fda8ee3be7
Parvatha reddy <56736219+raghava18@users.noreply.github.com>
2026-07-17 00:15:36 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route("/")
+def home():
     return 'Ome Shree Poojaya Raghavendra Satya Dharma Ratayacha bhajatam kalparukshaya Namatama Kamdhenuve!!!'
 
-if __name__ == '__main__':
-    app.run()
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
