1d8e44c7d9b9f7fc8b1c6195e096736653c94421
Parvatha reddy <56736219+raghava18@users.noreply.github.com>
2026-07-16 23:32:39 +0530

    Fix greeting message by adding an exclamation mark

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Ome Shree Poojaya Raghavendra Satya Dharma Ratayacha bhajatam kalparukshaya Namatama Kamdhenuve!'
+    return 'Ome Shree Poojaya Raghavendra Satya Dharma Ratayacha bhajatam kalparukshaya Namatama Kamdhenuve!!'
 
 if __name__ == '__main__':
     app.run()
